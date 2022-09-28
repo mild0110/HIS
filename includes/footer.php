4dd6c7691e339f2035c5266a833e1a55e0fab847
GeykZone <matildogeykson@gmai.com>
2022-09-28 22:55:50 +0800

    New commit

--- includes/footer.php
+++ includes/footer.php
@@ -16,8 +16,6 @@
   // SideNav Button Initialization
   $(".button-collapse").sideNav();
   // SideNav Scrollbar Initialization
-  var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-  var ps = new PerfectScrollbar(sideNavScrollbar);
 </script>
 
 </html>
